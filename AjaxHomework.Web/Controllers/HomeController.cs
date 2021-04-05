872f0106bb7a4fae822b0499e390fc50424574d0
Nechama Turin <nturin398@gmail.com>
2021-04-05 17:23:15 -0400

    Add project files.

---
+++ AjaxHomework.Web/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using AjaxHomework.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AjaxHomework.Data;
+
+namespace AjaxHomework.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private string _connectionString = @"Data Source=.\sqlexpress;Initial Catalog=PeopleDb;Integrated Security=true;";
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public IActionResult Add(Person p)
+        {
+            var db = new PeopleDb(_connectionString);
+            db.AddPerson(p);
+            return Json(p);
+        }
+
+        public IActionResult Edit(Person p)
+        {
+            var db = new PeopleDb(_connectionString);
+            db.EditPerson(p);
+            return Json(p);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var db = new PeopleDb(_connectionString);
+            db.DeletePerson(id);
+            return Json(id);
+        }
+
+        public IActionResult GetAll()
+        {
+            var db = new PeopleDb(_connectionString);
+            var ppl = db.GetAll();
+            return Json(ppl);
+        }
+
+
+
+    }
+}
---
+++ AjaxHomework.Web/wwwroot/js/Index.js
@@ -0,0 +1,77 @@
+﻿$(() => {
+
+    function fillTable() {
+        $("tbody").empty();
+        $.get('/home/getall', function (ppl) {
+            ppl.forEach(p => {
+                $("tbody").append(`
+<tr>
+    <td>${p.firstName}</td>
+    <td>${p.lastName}</td>
+    <td>${p.age}</td>
+    <td>
+      <button class="btn btn-outline-success edit-button" id="edit" data-id="${p.id}" data-first-name="${p.firstName
+                    }" data-last-name="${p.lastName}" data-age="${p.age}">Edit</button>
+    </td>
+    <td>
+      <button class="btn btn-outline-danger delete-button" id="delete" data-id="${p.id}">Delete</button>
+    </td>
+</tr>`);
+            });
+        });
+    }
+
+    fillTable();
+
+    $("#add-btn").on('click', function () {
+        let firstName = $("#first-name").val();
+        let lastName = $("#last-name").val();
+        let age = $("#age").val();
+
+        $("#first-name").val('');
+        $("#last-name").val('');
+        $("#age").val('');
+
+        $.post('/home/add', { firstName, lastName, age }, function (p) {
+            fillTable();
+        });
+
+    });
+
+    $("tbody").on('click', '#edit', function() {
+        let firstName = $(this).data('first-name');
+        let lastName = $(this).data('last-name');
+        let age = $(this).data('age');
+        let id = $(this).data('id');
+        $("#first-name-edit").val(firstName);
+        $("#last-name-edit").val(lastName);
+        $("#age-edit").val(age);
+        $("#id-edit").val(id);
+        $('.modal').modal();
+        fillTable();
+    })
+
+    $("#save-btn").on('click', function() {
+            let firstName = $("#first-name-edit").val();
+            let lastName = $("#last-name-edit").val();
+            let age = $("#age-edit").val();
+            let id = $("#id-edit").val();
+
+            $.post('/home/Edit', { firstName, lastName, age, id }, function(p) {
+                
+            })
+        $('.modal').modal('hide');
+        fillTable();
+    })
+
+    $("tbody").on('click', '.delete-button', function () {
+        console.log('hello!!!');
+        let id = $(this).data('id');
+
+        $.post('home/Delete', {id}, function(id) {
+            fillTable();
+        })
+    })
+
+
+});
